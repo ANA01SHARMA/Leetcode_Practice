89ebe3dfc835e651de8f938bc188b5f64815843f
ANAMIKA SHARMA <142999188+ANA01SHARMA@users.noreply.github.com>
2026-08-10 22:16:31 +0530

    Time: 0 ms (100%), Space: 46.9 MB (54.92%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -1,10 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    TreeNode ans= null;
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null || root.val == val) return root;
-
-        if (val < root.val)
-            return searchBST(root.left, val);
-        else
-            return searchBST(root.right, val);
+        solve(root, val);
+        return ans;
     }
+
+    public void solve(TreeNode root, int value){
+        if(root==null)
+        return;
+        if(root.val==value){
+            ans=root;
+            return;
+        }
+        if(root.val>value){
+            solve(root.left, value);
+        }
+        if(root.val<value){
+            solve(root.right, value);
+        }
+        
+    } 
+
 }
