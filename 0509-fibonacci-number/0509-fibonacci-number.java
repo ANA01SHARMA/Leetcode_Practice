82563d19c8083a564aadac3ec54b7e70ab469916
ANAMIKA SHARMA <142999188+ANA01SHARMA@users.noreply.github.com>
2026-08-24 21:01:21 +0530

    Time: 11 ms (9.66%), Space: 42 MB (70.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,22 +1,8 @@
 class Solution {
-    static int fib(int n) {
-        if (n <= 1)
-            return n;
-
-        int prev2 = 0;
-        int prev1 = 1;
-
-        for (int i = 2; i <= n; i++) {
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
-        }
-
-        return prev1;
-    }
-
-    public static void main(String[] args) {
-        int n = 7;
-        System.out.println(fib(n));
+    public int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        
+        return fib(n-1)+fib(n-2);
     }
 }
